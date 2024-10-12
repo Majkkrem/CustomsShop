626fdd4c08ba24f21c0740582c81a588256153b5
Sendula Márk <marksendula@gmail.com>
2024-10-12 11:35:52 +0200

    Elvileg kész layout az oldalnak, js-ben az összes funkciógomb lekérdezése, css az oldal hátterán és navbar-on kívül kész

---
+++ main.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const changeCarLeftButton = document.getElementById('changeCarLeftButton');
+    const changeCarRightButton = document.getElementById('changeCarRightButton');
+    const changeTireLeftButton = document.getElementById('changeTireLeftButton');
+    const changeTireRightButton = document.getElementById('changeTireRightButton');
+
+    const exteriorGrey = document.getElementById('exteriorGrey');
+    const exteriorBlack = document.getElementById('exteriorBlack');
+    const exteriorBlue = document.getElementById('exteriorBlue');
+    const exteriorRed = document.getElementById('exteriorRed');
+
+    const tire21 = document.getElementById('tire21');
+    const tire22_1 = document.getElementById('tire22_1');
+    const tire22_2 = document.getElementById('tire22_2');
+    const tire22_3 = document.getElementById('tire22_3');
+
+    const interiorBlack = document.getElementById('interiorBlack');
+    const interiorGrey = document.getElementById('interiorGrey');
+
+    const checkbox = document.getElementById('cbtest-19');
+    const checkbox2 = document.getElementById('cbtest-19_1');
+    const checkbox3 = document.getElementById('cbtest-19_2');
+    const checkbox4 = document.getElementById('cbtest-19_3');
+    const checkbox5 = document.getElementById('cbtest-19_4');
+    const checkbox6 = document.getElementById('cbtest-19_5');
+
+    const totalPrice = document.getElementById('totalPrice');
+
+
+
+});
